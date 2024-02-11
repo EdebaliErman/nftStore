63624d5a55d86ce064142ed666448700bbea3e8f
EdebaliErman <edebalierman@gmail.com>
2024-02-11 17:28:55 +0300

    hello

---
+++ git_push.cjs
@@ -0,0 +1,30 @@
+const { exec } = require('child_process');
+
+const commitMessage = process.argv[2] || 'Varsayılan açıklama';
+
+// Git add
+exec('git add .', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Git add hata: ${error}`);
+        return;
+    }
+    console.log(`Git add çıktı: ${stdout}`);
+
+    // Git commit
+    exec(`git commit -m "${commitMessage}"`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Git commit hata: ${error}`);
+            return;
+        }
+        console.log(`Git commit çıktı: ${stdout}`);
+
+        // Git push
+        exec('git push', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Git push hata: ${error}`);
+                return;
+            }
+            console.log(`Git push çıktı: ${stdout}`);
+        });
+    });
+});
